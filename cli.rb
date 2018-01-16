f5eb1fc1b03a4a953b96a67a095ff6b1ce9935c5
Ben Elgar <me@benelgar.com>
2018-01-15 18:30:25 -0800

    Update sha

--- cli.rb
+++ cli.rb
@@ -2,7 +2,7 @@ class Cli < Formula
   desc "The lattice command line tool"
   homepage "https://github.com/mlab-lattice/cli"
   url "https://s3-us-west-2.amazonaws.com/lattice-binaries/lattice"
-  sha256 "76b936760eef6e8371a35f5c8e096cf6d5818ca4070bca4f7b6780249e2caad6"
+  sha256 "6ef8f15962418c8e02ec35d7da0d210051c6ab7ebeba945e246158bbafa22846"
   version "0.1.0"
 
   bottle :unneeded
