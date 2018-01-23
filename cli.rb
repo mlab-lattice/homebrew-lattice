800a74607c34ef84ada80606fc006751e1f14b71
Ben Elgar <me@benelgar.com>
2018-01-22 17:04:12 -0800

    Bump version

--- cli.rb
+++ cli.rb
@@ -2,8 +2,8 @@ class Cli < Formula
   desc "The lattice command line tool"
   homepage "https://github.com/mlab-lattice/cli"
   url "https://s3-us-west-2.amazonaws.com/lattice-binaries/lattice"
-  sha256 "ab06bbacb682f23418c9e020e1fdfcd23f6c9e61cd3ac7f917ea3658c6f7d884"
-  version "0.1.5"
+  sha256 "80a9f5c6311bc102e42d0991530228095588c0b28f6584967b83e2c7be139864"
+  version "0.1.6"
 
   bottle :unneeded
 
