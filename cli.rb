c4321061a9caea43ec61d672b036a7092d2c6aca
concourse <concourse@mlab.com>
2018-01-30 07:57:34 +0000

    Bump to version 0.1.8

--- cli.rb
+++ cli.rb
@@ -2,7 +2,7 @@ class Cli < Formula
   desc 'The lattice command line tool'
   homepage 'https://github.com/mlab-lattice/cli'
   url 'https://s3-us-west-2.amazonaws.com/lattice-binaries/lattice_darwin_amd64_v0.1.8'
-  sha256 'ae835c7516e60d6763a93877c1ca5324d01e282706866518d1c93bf029e9e8a1'
+  sha256 '352239506dce6e655980d34554811d82a7a9484c91d6b172ac5a32af59459f6b'
   version '0.1.8'
 
   bottle :unneeded
