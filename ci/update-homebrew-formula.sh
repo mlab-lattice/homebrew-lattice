2969228a3e75f5a7bedde979c51799c4eb694fa8
Ben Elgar <me@benelgar.com>
2018-01-30 00:04:35 -0800

    Fix bug with using wrong shasum

--- ci/update-homebrew-formula.sh
+++ ci/update-homebrew-formula.sh
@@ -5,10 +5,8 @@ set -o pipefail
 set -o nounset
 
 BINARY_TAG=$(cat ../cli-metadata/tag)
-LINUX_BINARY_FILENAME=$(cat ../cli-metadata/linux_filename)
 DARWIN_BINARY_FILENAME=$(cat ../cli-metadata/darwin_filename)
-LINUX_BINARY_SHASUM=$(cat ../cli-metadata/linux_shasum)
-DARWIN_BINARY_SHASUM=$(cat ../cli-metadata/linux_shasum)
+DARWIN_BINARY_SHASUM=$(cat ../cli-metadata/darwin_shasum)
 
 cat > ./cli.rb << EOF
 class Cli < Formula
