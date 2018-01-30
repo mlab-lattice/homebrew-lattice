24340bd0d95d1fb75bee910bc2547d7e83e1f165
concourse <concourse@mlab.com>
2018-01-30 07:50:51 +0000

    Bump to version v0.1.7

--- cli.rb
+++ cli.rb
@@ -1,14 +1,14 @@
 class Cli < Formula
   desc 'The lattice command line tool'
   homepage 'https://github.com/mlab-lattice/cli'
-  url 'https://s3-us-west-2.amazonaws.com/lattice-binaries/lattice_darwin_amd64_v0.1.3'
-  sha256 '2419aba1ee511da10db48cdedfc8dc095731c6ce9dcfc4c67f994a1a3d8af22c'
-  version 'v0.1.3'
+  url 'https://s3-us-west-2.amazonaws.com/lattice-binaries/lattice_darwin_amd64_vv0.1.7'
+  sha256 '9184f6be233a0f66775242dd86651229b93efc60cf34e75c5948e0135d9dee15'
+  version 'v0.1.7'
 
   bottle :unneeded
 
   def install
-    prefix.install 'lattice_darwin_amd64_v0.1.3' 'lattice'
+    prefix.install 'lattice_darwin_amd64_vv0.1.7' 'lattice'
     bin.install 'lattice'
   end
 end
