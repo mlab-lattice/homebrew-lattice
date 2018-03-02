b4c1fe2d26015a32bfbace31637f93523313ed83
Ben Elgar <me@benelgar.com>
2018-03-01 17:03:19 -0800

    Fix bug whereby committing was broken if changes

--- ci/update-homebrew-formula.sh
+++ ci/update-homebrew-formula.sh
@@ -3,6 +3,7 @@
 set -o errexit
 set -o pipefail
 set -o nounset
+set -o xtrace
 
 BINARY_TAG=$(cat ../cli-metadata/tag)
 DARWIN_BINARY_FILENAME=$(cat ../cli-metadata/darwin_filename)
@@ -29,10 +30,9 @@ git config --local user.email concourse@mlab.com
 git config --local user.name concourse
 
 # if changes
-if [[ `git status --porcelain` ]]; then
-  git add ./cli.rb
+if [[ "$(git status --porcelain)" ]]; then
+  git add './cli.rb'
   git commit -m "Bump to version $BINARY_TAG"
 fi
 
-cp -a ./. ../updated-homebrew-repo
-
+cp -a . ../updated-homebrew-repo
