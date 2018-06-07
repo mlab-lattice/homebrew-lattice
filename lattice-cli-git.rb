543e77b432197f151f841cd269df4487b5d69233
concourse <concourse@mlab.com>
2018-06-07 18:18:45 +0000

    Bump to version 0.2.35-rc-10-g3024d6a

--- lattice-cli-git.rb
+++ lattice-cli-git.rb
@@ -1,14 +1,14 @@
 class LatticeCliGit < Formula
   desc 'The lattice command line tool (HEAD release)'
   homepage 'https://github.com/mlab-lattice/cli'
-  url 'https://s3-us-west-2.amazonaws.com/lattice-staging-binaries/lattice_darwin_amd64_v0.2.35-rc-9-gb2775b4'
-  sha256 '2f0377616ca85a295f8cc3c4cbb7658a00b1393739631295f98313feee692e6b'
-  version '0.2.35-rc-9-gb2775b4'
+  url 'https://s3-us-west-2.amazonaws.com/lattice-staging-binaries/lattice_darwin_amd64_v0.2.35-rc-10-g3024d6a'
+  sha256 'b8df290fafb0e954effc243bcd1a07681fc791c09084a3128d443c2d924027eb'
+  version '0.2.35-rc-10-g3024d6a'
 
   bottle :unneeded
 
   def install
-    mv 'lattice_darwin_amd64_v0.2.35-rc-9-gb2775b4', 'lattice-git'
+    mv 'lattice_darwin_amd64_v0.2.35-rc-10-g3024d6a', 'lattice-git'
     bin.install 'lattice-git'
   end
 end
