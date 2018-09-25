c0d325d8991c0e89504317b105d98a2a970c6125
Tim Fogarty <tim@tfogo.com>
2018-09-25 15:52:00 -0700

    Fix latticectl formula

--- latticectl.rb
+++ latticectl.rb
@@ -1,7 +1,7 @@
 class Latticectl < Formula
   desc 'The latticectl command line tool'
   homepage 'https://github.com/mlab-lattice/lattice'
-  url 'https://s3-us-west-2.amazonaws.com/lattice-binaries/latticectl_darwin_amd_v0652d73'
+  url 'https://s3-us-west-2.amazonaws.com/lattice-binaries/latticectl_darwin_amd64_v0652d73'
   sha256 '7b365b24270a6c809079c5127999a4f41ffc5c90ef491c1cb5098a6bd47e6983'
   version '0.2.38'
 
