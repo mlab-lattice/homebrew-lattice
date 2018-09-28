4ef82a96cd0614764a3a8080f8c17edd1c3dea2e
concourse <concourse@mlab.com>
2018-09-28 21:42:09 +0000

    Bump to version v0.1.1-6-g5517e9d

--- latticectl.rb
+++ latticectl.rb
@@ -1,14 +1,14 @@
 class Latticectl < Formula
   desc 'The latticectl command line tool'
   homepage 'https://github.com/mlab-lattice/lattice'
-  url 'https://s3-us-east-2.amazonaws.com/lattice-binaries-staging/../cli-binaries/latticectl_darwin_amd64_vv0.1.1-80-gedf829f'
-  sha256 '5da8f991ca6cb50741da9d5f8e23d3f7c9fa2523703ee2fbfa9200a6fab13ffa'
-  version 'v0.1.1-80-gedf829f'
+  url 'https://s3-us-east-2.amazonaws.com/lattice-binaries-staging/../cli-binaries/latticectl_darwin_amd64_vv0.1.1-6-g5517e9d'
+  sha256 '9e4ca629f3b4a8436bc2fff98bda47955d49abf7f376e7c936d7d0b8fbbb6430'
+  version 'v0.1.1-6-g5517e9d'
 
   bottle :unneeded
 
   def install
-    mv '../cli-binaries/latticectl_darwin_amd64_vv0.1.1-80-gedf829f', 'latticectl'
+    mv '../cli-binaries/latticectl_darwin_amd64_vv0.1.1-6-g5517e9d', 'latticectl'
     bin.install 'latticectl'
   end
 end
